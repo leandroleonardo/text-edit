011b3fe71fecdc37b113330d5a88f3a85b80776e
Leandro Assarice <leandro.assarice@gmail.com>
2021-01-22 16:12:01 -0300

    optimization

--- script.js
+++ script.js
@@ -1,68 +1,70 @@
+let text = () => document.getElementById("input-text")
+
 function textUpdate(input) {
     let textId = document.getElementById("display-text")
     textId.innerText = input.value;
 }
 
-function textDisplay() {
-    let text = document.getElementById("input-text")
-    return text
-}
-
 function color(color) {
-    let text = textDisplay() 
-    text.style.color = color.value
+    text().style.color = color.value
 }
 
 function background_color(color_b) {
-    let text = textDisplay()
-    text.style.backgroundColor = color_b.value
+    text().style.backgroundColor = color_b.value
 }
 
 function selectFont(textFont) {
-    let text = textDisplay()
 
-    if(textFont.value === "arial") {
-        text.style.fontFamily = "Arial"
-    }
-    else if(textFont.value === "time") {
-        text.style.fontFamily = "Times New Roman"
-    }
-    else if(textFont.value === "franklin") {
-        text.style.fontFamily = "Franklin Gothic Medium"
+    switch(textFont.value) {
+        
+        case "arial":
+            text().style.fontFamily = "Arial"
+        break;
+
+        case "time":
+            text().style.fontFamily = "Times New Roman"
+        break;
+
+        case "franklin":
+            text().style.fontFamily = "Franklin Gothic Medium"
+        break;
+
     }
 }
 
 function selectRange(porcent) {
-    let text = textDisplay()
-    text.style.fontSize = `${porcent.value}px`
+    text().style.fontSize = `${porcent.value}px`
 }
 
-let cont = 0 
+function theme() {
 
-function theme(click) {
-    
-    /* let text = textDisplay() */
+    let verification = document.getElementById("theme")
     let textColorLetter = document.getElementsByClassName("colorText")
-    let valor = parseInt(click.value)
-    
-    cont += valor
-    
-    if(cont%2 == 0) {
+
+    if(verification.value === "true") {
+
         document.body.style.backgroundColor = "#242120"
+        
         for(let i=0; i<textColorLetter.length; i++) {
-            console.log(i)
             textColorLetter[i].style.color = "white"
         }
+
+        verification.value = "false"
+        
     } else {
+    
         for(let i=0; i<textColorLetter.length; i++) {
-            console.log(i)
             textColorLetter[i].style.color = "black"
         }
+
         document.body.style.backgroundColor = "white"
-    }
+
+        verification.value = "true"
+    } 
 }
 
 function printText() {
+
     let textColorLetter = document.getElementsByClassName("colorText")
    
     for(let i=0; i<textColorLetter.length; i++) {
@@ -72,6 +74,7 @@ function printText() {
     document.getElementById("theme").style.opacity = "0"
 
     alert("Ative os elementos gráficos de plano de fundo")
+
     window.print()
 
     for(let i=0; i<textColorLetter.length; i++) {
